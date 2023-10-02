1cb4dc590e8011b13da740c11958724da1644937
Markus Ylitalo <markus.ylitalo@cgi.com>
2023-10-02 17:17:38 +0300

    Fix some compiler errors

--- packages/react-consumer-app/src/components/ContactForm.tsx
+++ packages/react-consumer-app/src/components/ContactForm.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { MyRow, MyCol, MyTextInput, MyButton } from "react-library"
+import { MyTextInputCustomEvent } from "stencil-library"
 
 const ContactForm = () => {
   const [firstName, setFirstName] = useState('')
@@ -9,13 +10,13 @@ const ContactForm = () => {
   return (
     <MyRow>
       <MyCol size={6}>
-        <MyTextInput label='First name' name='first-name' value={firstName} onValueChanged={(e) => setFirstName(e.target.value)} />
+        <MyTextInput label='First name' name='first-name' value={firstName} onValueChanged={(e: MyTextInputCustomEvent<string>) => setFirstName(e.target.value)} />
       </MyCol>
       <MyCol size={6}>
-        <MyTextInput label='Last name' name='last-name' value={lastName} onValueChanged={(e) => setLastName(e.target.value)} />
+        <MyTextInput label='Last name' name='last-name' value={lastName} onValueChanged={(e: MyTextInputCustomEvent<string>) => setLastName(e.target.value)} />
       </MyCol>
       <MyCol size={12}>
-        <MyTextInput label='Street address' name='street-address' value={address} onValueChanged={(e) => setAddress(e.target.value)} />
+        <MyTextInput label='Street address' name='street-address' value={address} onValueChanged={(e: MyTextInputCustomEvent<string>) => setAddress(e.target.value)} />
       </MyCol>
       <MyCol size={12}>
         <MyButton>Submit</MyButton>
